bf8ba4cdc1432da0d7a1f4e79d73880912b18039
Aleksandr <Avshef451987@gmail.com>
2024-03-29 00:52:38 +0500

    test

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        react(),
+        VitePWA({
+            includeAssets: ["favicon.ico", "apple-touch-icon.png", "logo.svg"],
+            manifest: {
+                name: "Name App",
+                short_name: "Short Name",
+                description: "Description",
+                theme_color: "#ffffff",
+                icons: [
+                    {
+                        src: "android-chrome-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "android-chrome-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                ],
+            },
+        }),
+    ],
+});
